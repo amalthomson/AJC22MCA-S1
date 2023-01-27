4d2e4a61142b4e65abcc310df27fc055c5bc1c52
amalthomson <117607207+amalthomson@users.noreply.github.com>
2023-01-27 07:05:12 +0530

    updates

--- 20MCA131 - Programming/CO5/CO5_05.py
+++ 20MCA131 - Programming/CO5/CO5_05.py
@@ -1,4 +1,5 @@
-# Data to be inserted
+
+import csv
 data = [{'Name': 'John', 'Age': 25, 'Country': 'United States'},
 {'Name': 'Mike', 'Age': 32, 'Country': 'Canada'},
 {'Name': 'Sarah', 'Age': 35, 'Country': 'United Kingdom'}]
@@ -6,8 +7,9 @@ with open('people.csv', 'w') as csvfile:
     headernames = ['Name', 'Age', 'Country']
     csvwriter = csv.DictWriter(csvfile, fieldnames=headernames)
     csvwriter.writeheader()
-for row in data:
-    csvwriter.writerow(row)
-reader = csv.DictReader(csvfile)
-for row in reader:
-    print(row)
+    for row in data:
+        csvwriter.writerow(row)
+with open('people.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+            print(row)
